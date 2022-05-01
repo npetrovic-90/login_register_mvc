cd2725648da4d75c08d146555870dcaa7423ad92
Nemanja Petrovic <nemanja90petrovic@hotmail.com>
2022-05-01 11:04:45 +0200

    Setting up MVC framework - project created

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+//Database params
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','mvc_php_native');
+
+//root of our app
+define('APPROOT',dirname(dirname(__FILE__)));
+
+//URLROOT
+define('URLROOT','localhost/mvc_php_native');
+
+//Sitename
+define('SITENAME','MVC_PHP_NATIVE');
---
+++ app/views/pages/index.php
@@ -0,0 +1,9 @@
+
+<?php
+
+foreach($data['users'] as $user){
+
+    echo "Information:".$user->user_name." ".$user->user_email."<br>";
+}
+
+?>
